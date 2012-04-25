b5dab5dc58105099858e8c9758e1e4f524683d59
Jon Kinney <jonkinney@gmail.com>
2012-04-25 11:40:24 -0500

    Fixes readme and test
    - Adjust readme to have the correct singular version of the class name
    in the code block
    - change the test to a valid assert_equal assertion

--- clone_war_test.rb
+++ clone_war_test.rb
@@ -2,6 +2,6 @@ require 'minitest/autorun'
 
 class TestCloneWar < MiniTest::Unit::TestCase
   def test_should_not_be_at_war
-		assert_false CloneWar.new.war?
+		assert_equal CloneWar.new.war?, false
   end
 end
