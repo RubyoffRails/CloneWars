cecacbfbb52e917e0c258106f6d37e3c9bf3efbf
Peter Aronoff <telemachus@arpinum.org>
2012-04-25 13:12:28 -0400

    Not at war

--- clone_war_test.rb
+++ clone_war_test.rb
@@ -1,7 +1,8 @@
+require_relative 'clone_war'
 require 'minitest/autorun'
 
 class TestCloneWar < MiniTest::Unit::TestCase
   def test_should_not_be_at_war
-		assert_false CloneWar.new.war?
+    refute CloneWar.new.war?
   end
 end
