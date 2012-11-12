816a039a5cf71a555e270c0c61885f21c1341d4d
Sebastian Bachmann <sebastianbachmann@macnews.de>
2012-11-12 21:50:32 +0100

    new test code but it does not work

--- clone_war_test.rb
+++ clone_war_test.rb
@@ -2,6 +2,6 @@ require 'minitest/autorun'
 
 class TestCloneWar < MiniTest::Unit::TestCase
   def test_should_not_be_at_war
-		assert false, CloneWar.new.war?
+    assert_equal false, CloneWar.new.war?
   end
 end
