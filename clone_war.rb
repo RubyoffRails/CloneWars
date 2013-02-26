4c7208cd9dd3c7a558be7e1e7054d29d7f7ab610
rssll <grgrsslldlln@gmail.com>
2013-02-25 21:39:05 -0600

    Feature: Not at war

---
+++ clone_war.rb
@@ -0,0 +1,9 @@
+class CloneWar
+  def war?
+    false
+  end
+end
+
+# Russell Dillin
+# Tests pass with:
+# 'bundle exec rake' and 'ruby clone_war_test.rb'
--- clone_war_test.rb
+++ clone_war_test.rb
@@ -1,7 +1,14 @@
+require 'rubygems'
+gem 'minitest'
 require 'minitest/autorun'
+require './clone_war.rb'
 
 class TestCloneWar < MiniTest::Unit::TestCase
+  def setup
+    @clone_war = CloneWar.new
+  end
+
   def test_should_not_be_at_war
-    assert_equal false, CloneWar.new.war?
+    assert_equal false, @clone_war.war?
   end
 end
