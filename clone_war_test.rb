fa75b563b6f70f0910dac277bee29d36e5ac77e6
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-03 23:37:14 -0400

    Update clone_war_test.rb

--- clone_war_test.rb
+++ clone_war_test.rb
@@ -4,6 +4,5 @@ require './clone_war'
 class TestCloneWar < MiniTest::Unit::TestCase
   def test_should_not_be_at_war
     assert_equal false, CloneWar.new.war?
-    assert_equal true, CloneWar.new.stormtroopers?
   end
 end
